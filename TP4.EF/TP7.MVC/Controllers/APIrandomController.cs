08cecd9eb4dc850346e25e085ed9808f3059c02b
Gomez Kevin <kevindelnormal@gmail.com>
2022-06-22 16:59:44 -0300

    Primera parte del TP8 realizado Consumo de valores de una API publica

---
+++ TP4.EF/TP7.MVC/Controllers/APIrandomController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using Newtonsoft.Json;
+using TP7.MVC.Models;
+
+namespace TP7.MVC.Controllers
+{
+    public class APIrandomController : Controller
+    {
+        // GET: APIrandom
+        public ActionResult Index()
+        {
+            var url = "https://randomuser.me/api/?results=10";
+            WebClient wc = new WebClient();
+            var datos = wc.DownloadString(url);
+            var resultado = JsonConvert.DeserializeObject<Resultado>(datos); 
+            return View(resultado);
+        }
+    }
+}
